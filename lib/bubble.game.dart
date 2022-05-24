c365ed8c59172993c13c352fb1947e457b49a5ed
Michał Dziwota <mdziwota@sii.pl>
2022-05-24 10:30:52 +0200

    architerture fix

--- lib/bubble.game.dart
+++ lib/bubble.game.dart
@@ -1,11 +1,8 @@
 import 'package:bubble_fight/player.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
 import 'package:flame/palette.dart';
-import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 import 'package:flutter/services.dart';
 
 class BubbleGame extends FlameGame with HasDraggables {
@@ -17,23 +14,10 @@ class BubbleGame extends FlameGame with HasDraggables {
     margin: const EdgeInsets.only(left: 60, bottom: 60),
   );
 
-  // late final Player player;
-  // late final Player player = Player()
-  //   ..position = size / 2
-  //   ..width = 50
-  //   ..height = 100
-  //   ..anchor = Anchor.center;
+  final playerController = PlayerController();
+  late final Player player;
 
   double maxSpeed = 300.0;
-
-  late RiveComponent playerComponent;
-  final playerMovementAnimationController = OneShotAnimation(
-    'movement',
-    autoplay: false,
-  );
-
-  late final _playerArtboard =
-      loadArtboard(RiveFile.asset('assets/ball_player.riv'));
 
   @override
   Future<void> onLoad() async {
@@ -42,56 +26,18 @@ class BubbleGame extends FlameGame with HasDraggables {
       DeviceOrientation.portraitDown,
     ]);
     await super.onLoad();
-    removeBackground();
-    final playerArtboard = await _playerArtboard;
-    playerComponent = RiveComponent(artboard: playerArtboard)
-      ..size = Vector2.all(100)
-      ..anchor = Anchor.center;
-    // var playerController = OneShotAnimation('idle', autoplay: true);
-    playerArtboard.addController(playerMovementAnimationController);
-    add(playerComponent);
+
+    player = await Player.create(playerController);
+    add(player);
     add(joystick);
-  }
-
-  Future<void> removeBackground() async {
-    final playerArtboard = await _playerArtboard;
-    playerArtboard.forEachComponent((p0) {
-      if (p0 is Fill) {
-        final isBackground = p0.paint.color.value == 0xff977bc3;
-        if (isBackground) {
-          p0.paint.color = Colors.transparent;
-        }
-      }
-
-      if (p0 is Stroke) {
-        final isShadow = p0.paint.color.value == 0xff844da3;
-        if (isShadow) {
-          p0.paint.color = Colors.transparent;
-        }
-      }
-
-      if (p0 is Shape) {
-        final isShadow = p0.name == 'shadow';
-        if (isShadow && p0.fills.isNotEmpty) {
-          p0.fills.first.paint.color = Colors.transparent;
-        }
-      }
-    });
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    // if (!joystick.delta.isZero() && activeCollisions.isEmpty) {
     if (!joystick.delta.isZero()) {
-      // _lastSize.setFrom(size);
-      // _lastTransform.setFrom(transform);
-      playerComponent.position.add(joystick.relativeDelta * maxSpeed * dt);
-      playerComponent.angle = joystick.delta.screenAngle();
-      // playerMovementAnimationController.isActive = true;
-    } else {
-      // print('reset');
-      playerMovementAnimationController.reset();
+      player.position.add(joystick.relativeDelta * maxSpeed * dt);
+      player.angle = joystick.delta.screenAngle();
     }
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -3,22 +3,60 @@ import 'package:flame_rive/flame_rive.dart';
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-class Player extends PositionComponent {
-  static final _paint = Paint()..color = Colors.white;
+class PlayerController {}
 
-  final Artboard _artboard;
+class Player extends RiveComponent {
+  final PlayerController controller;
 
-  Player._(this._artboard);
+  Player({
+    required super.artboard,
+    required this.controller,
+  });
 
-  static Future<Player> create() async {
-    final artboard = await loadArtboard(RiveFile.asset('assets/player.riv'));
-    return Player._(artboard);
+  static Future<Player> create(PlayerController controller) async {
+    final artboard =
+        await loadArtboard(RiveFile.asset('assets/ball_player.riv'));
+
+    final playerMovementAnimationController = OneShotAnimation(
+      'movement',
+      autoplay: false,
+    );
+    artboard.addController(playerMovementAnimationController);
+    return Player(
+      artboard: artboard,
+      controller: controller,
+    );
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    // _artboard.addComponent(this);
-    canvas.drawCircle(Offset.zero, 20.0, _paint);
+    size = Vector2.all(50);
+    anchor = Anchor.center;
+  }
+
+  Future<void> removeBackground() async {
+    artboard.forEachComponent((p0) {
+      if (p0 is Fill) {
+        final isBackground = p0.paint.color.value == 0xff977bc3;
+        if (isBackground) {
+          p0.paint.color = Colors.transparent;
+        }
+      }
+
+      if (p0 is Stroke) {
+        final isShadow = p0.paint.color.value == 0xff844da3;
+        if (isShadow) {
+          p0.paint.color = Colors.transparent;
+        }
+      }
+
+      if (p0 is Shape) {
+        final isShadow = p0.name == 'shadow';
+        if (isShadow && p0.fills.isNotEmpty) {
+          p0.fills.first.paint.color = Colors.transparent;
+        }
+      }
+    });
   }
 }
