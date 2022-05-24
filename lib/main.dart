44aa3c10ddab2b5f2fb662ce53521a05388381ec
Michał Dziwota <mdziwota@sii.pl>
2022-05-24 10:55:27 +0200

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:bubble_fight/bubble.game.dart';
+import 'package:bubble_fight/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(
     GameWidget(
       game: BubbleGame(),
     ),
+  );
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
