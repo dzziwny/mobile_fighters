8bd5647ba66589cd95ddc56a98e3e2cdc6a9801f
Michał Dziwota <mdziwota@sii.pl>
2022-09-11 10:26:38 +0200

    added debug info widget

--- app/lib/home.screen.dart
+++ app/lib/home.screen.dart
@@ -26,32 +26,81 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               Navigator.of(context).push<void>(
                 MaterialPageRoute<void>(
-                  builder: (BuildContext context) => Stack(
-                    children: [
-                      GameWidget(
-                        game: BubbleGame(gameId: controller.text),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: Alignment.topLeft,
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              client.leaveGame();
-                              Navigator.of(context).pop();
-                            },
-                            icon: const Icon(Icons.exit_to_app),
-                            label: const Text('Leave'),
-                          ),
+                  builder: (BuildContext context) => Scaffold(
+                    body: Stack(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 800.0,
+                              child: GameWidget(
+                                game: BubbleGame(gameId: controller.text),
+                              ),
+                            ),
+                            const Expanded(
+                              child: DebugInfo(),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                        Container(
+                          margin: const EdgeInsets.all(8.0),
+                          alignment: Alignment.topLeft,
+                          child: _LeaveButton(client: client),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class DebugInfo extends StatelessWidget {
+  const DebugInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Players: [bla, blab, dajsk]',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _LeaveButton extends StatelessWidget {
+  const _LeaveButton({
+    Key? key,
+    required this.client,
+  }) : super(key: key);
+
+  final ServerClient client;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        client.leaveGame();
+        Navigator.of(context).pop();
+      },
+      icon: const Icon(Icons.exit_to_app),
+      label: const Text('Leave'),
+      style: const ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(Colors.red),
       ),
     );
   }
